86bb01755bc86c71474df8a7a79d167ee0a965fe
wenju.li <wenju.li@deepctr.cn>
2023-12-13 06:55:19 +0800

    expand dict num

--- cutword/cutword.py
+++ cutword/cutword.py
@@ -13,13 +13,34 @@ root_path = os.path.dirname(os.path.realpath(__file__))
 class Cutter:
     """Unigram tokenizer with Aho-Corasick automaton
     """
-    def __init__(self, dict_name="dict.txt"):
+    def __init__(
+            self, 
+            dict_name="dict.txt", 
+            union_name="unionwords.txt",
+            custom_dict_path=None
+        ):
+        '''
+        # dict_name:默认的词典的名称
+        # need_mid_long  是否加载中等长度的词典，
+                        该词典中会包含中等长度的短语，
+                        如：一卦千金，谨防疏漏等。
+                        也可能包含一些短的词语，
+                        但是这些词语不包含在基本的词典中。
+                        如明神宗，玉带，采苓等
+        # need_long   是否加载长的词典， 该词典中会包含比较长的短语，
+                      如：阅卷阶段发现作答内容雷同，进取的人生态度等
+        # custom_dict_path  用户自定义词典
+        '''
         dict_path = os.path.join(root_path, dict_name)
         self._pieces = {}
-        for line in open(dict_path):
-            line = line.strip()
-            word, freq, pos = line.split()
-            self._pieces[word] = [int(freq) + 1e-10, pos]
+        self._load_dict(dict_path)
+        union_path = os.path.join(root_path, union_name)
+        self._load_dict(union_path)
+        if custom_dict_path:
+            if not os.path.exists(custom_dict_path):
+                raise Exception("custom dict path not exists: %s" % custom_dict_path)
+            self._load_dict(custom_dict_path)
+
        
         # Aho-Corasick automaton
         log_total = np.log(sum([_[0] for _ in self._pieces.values()]))
@@ -27,6 +48,21 @@ class Cutter:
         for k, v in self._pieces.items():
             self._automaton.add_word(k, (len(k), np.log(v[0]) - log_total, v[1]))
         self._automaton.make_automaton()
+
+    def _load_dict(self, file_path):
+        print("Loading dictionaries from %s" % file_path)
+        for line in open(file_path):
+            line = line.strip()
+            r = line.split()
+            if len(r) >= 2:
+                self._pieces[r[0]] = [
+                    int(r[1]) + 1e-10, 
+                    r[2] if len(r) > 2 else ''
+                ]
+            else:
+                raise ValueError(
+                    'invalid dict file format', line
+                )
 
 
     def _tokenize(self, text):
@@ -73,7 +109,7 @@ class Cutter:
 
 if __name__ == "__main__":
     tokenizer = Cutter()
-    text = "张家口市民王北京如是说"
+    text = "小明来到中国科学院进修"
     res = tokenizer.cutword(text)
     print(res)
 
