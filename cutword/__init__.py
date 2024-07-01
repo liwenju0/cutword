434af690e93bc271ca707ebbb3798ceb0b19abbc
wenju.li <wenju.li@deepctr.cn>
2024-07-01 13:24:35 +0800

    当没有torch时，仍然可使用分词功能

--- cutword/__init__.py
+++ cutword/__init__.py
@@ -1,5 +1,9 @@
 #! -*- coding: utf-8 -*-
-from .ner import NER
+try:
+    from .ner import NER
+except ModuleNotFoundError as e:
+    print(f"导入ner模块时，发生异常 {e}, 将不能使用ner功能")
+
 from .cutword import Cutter
 
 
